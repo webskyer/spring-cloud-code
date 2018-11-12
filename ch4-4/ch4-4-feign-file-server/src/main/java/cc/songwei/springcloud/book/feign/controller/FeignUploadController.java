02240df5ab9a2ac259c94c79cde921f7e3843dc0
Webskyer <webskyer@songwei.cc>
2018-11-12 18:41:23 +0800

    Add 4-4 code

---
+++ ch4-4/ch4-4-feign-file-server/src/main/java/cc/songwei/springcloud/book/feign/controller/FeignUploadController.java
@@ -0,0 +1,17 @@
+package cc.songwei.springcloud.book.feign.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class FeignUploadController {
+
+    @PostMapping(value = "/uploadFile/server", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String fileUploadServer(MultipartFile file ) throws Exception{
+        return file.getOriginalFilename();
+    }
+
+}
+
